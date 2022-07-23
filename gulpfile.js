522ad0488bb1c905c128559048be494b4ac72104
9NicolayT9 <nicolay.talash@mail.ru>
2022-07-23 19:59:33 +0300

    Changes

--- gulpfile.js
+++ gulpfile.js
@@ -7,33 +7,60 @@ const del = require('del');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
+const newer = require('gulp-newer');
 
 // Пути сохранения
 const paths = {
+    html: {
+        src: 'src/*.html',
+        dest: 'dist/'
+    },
     styles: {
         src: 'src/Styles/**/*.sass',
-        dest: 'dist/styles/'
+        dest: 'dist/Styles/'
     },
     scripts: {
         src: 'src/Scripts/**/*.js',
-        dest: 'dist/scripts/'
+        dest: 'dist/Scripts/'
+    },
+    img: {
+        src: 'src/Photos/*',
+        dest: 'dist/Photos/',
     }
 }
 
 // Очистка готовой папки
 function clean() {
-    return del(['dist'])
+    return del(['dist/*', '!dist/Photos'])
+}
+
+// Компилятор HTML
+function html() {
+    return gulp.src(paths.html.src)
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest((paths.html.dest)));
 }
 
 // Компилятор стилей
 function styles() {
     return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
     .pipe(sass())
-    .pipe(cleanCss())
+    .pipe(autoprefixer({
+        cascade: false
+    }))
+    .pipe(cleanCss({
+        level: 2
+    }))
     .pipe(rename({
         basename: 'main',
         suffix: '.min'
     }))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(paths.styles.dest))
 }
 
@@ -45,7 +72,19 @@ function scripts() {
     .pipe(babel())
     .pipe(uglify())
     .pipe(concat('main.min.js'))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(paths.scripts.dest))
+}
+
+
+// Сжатие картинок
+function img() {
+    return gulp.src(paths.img.src)
+    .pipe(newer(paths.img.dest))
+    .pipe(imagemin({
+        progressive: true
+    }))
+    .pipe(gulp.dest(paths.img.dest))
 }
 
 // Автоматический просмотр изменений
@@ -55,9 +94,11 @@ function watch() {
 }
 
 // Операции сборки
-const build = gulp.series(clean, gulp.parallel(styles, scripts), watch)
+const build = gulp.series(clean, html, gulp.parallel(styles, scripts, img), watch)
 
 exports.clean = clean
+exports.html = html
+exports.img = img
 exports.styles = styles
 exports.scripts = scripts
 exports.watch = watch
